c0f0c6c25e5b3b31732f127d0483ab326e37e1e7
tvergara <tverg419@gmail.com>
2021-09-03 16:09:52 -0400

    working backend

---
+++ controllers/users.js
@@ -0,0 +1,37 @@
+// routes for slug
+
+const express = require('express')
+const router = express.Router()
+
+const User = require('../models/User')
+
+router.get('/', (req, res, next) => {
+    User.find({})
+    .then((users) => res.json(users))
+    .catch(next)
+})
+
+router.get('/:id', (req, res, next) => {
+    User.findById(req.params.id)
+    .then((user) => res.json(user))
+    .catch(next)
+})
+
+router.post('/', (req, res , next) => {
+    User.create(req.body)
+    .then((user) => re.json(user))
+    .catch(next)
+})
+
+router.put('/:id', (req, res, next) => {
+    User.findByIdAndUpdate({_id: req.params.id}, req.body, {new: true})
+    .then((user) => res.json(user))
+    .catch(next)
+})
+
+router.delete('/:id', (req, res, next) => {
+    User.findOneAndDelete({_id: req.params.id})
+    .then((user) => res.json(user))
+    .catch(next)
+})
+module.exports = router
---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+const usersController = require('./controllers/users.js')
+const notesController = require('./controllers/notes.js')
+
+app.use('/api/users', usersController)
+app.use('/api/notes', notesController)
+
+const port = 4000
+app.listen(port, () => {
+    console.log("Connected Port 4000")
+})
