5eda4d0354265f4856c0a1ac5504339f40ae3587
aahzambardai <ahzam.bardai@outlook.com>
2021-09-04 12:04:32 -0500

    body parser added with limit 50mb

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const app = express()
 
 app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(bodyParser.json({limit: '50mb'}))
+app.use(bodyParser.urlencoded({extended: true, limit: '50mb'}))
 
 const usersController = require('./controllers/users.js')
 const notesController = require('./controllers/notes.js')
