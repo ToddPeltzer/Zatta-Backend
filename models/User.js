a55fc420189ee6cddccef231d5940f025d42b577
aahzambardai <ahzam.bardai@outlook.com>
2021-09-07 11:13:20 -0500

    default email

--- models/User.js
+++ models/User.js
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema({
         required: true},
     email: {
         type: String,
-        default: ""
+        default: ''
     },
     username: {
         type: String, 
